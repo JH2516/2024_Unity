28b9034679802c7c0e8fddc04860612a8f14fca5
JH2516 <kaizer2516@gmail.com>
2024-04-03 22:52:31 +0900

    DNF

--- week5/Assets/Script/Button.cs
+++ week5/Assets/Script/Button.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -23,8 +24,32 @@ public class StaticMeshGen : MonoBehaviour
     // Start is called before the first frame update
     void GenerateMesh()
     {
-        
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3 (Math.Cos(90), 0.0f, Math.Sin(90)),//1,4
+            new Vector3 (Math.Cos(162), 0.0f, Math.Sin(162)),//2,1,6
+            new Vector3 (Math.Cos(234), 0.0f, Math.Sin(234)),//3,3
+            new Vector3 (Math.Cos(306), 0.0f, Math.Sin(306)),//4,5
+            new Vector3 (Math.Cos(18), 0.0f, Math.Sin(18)),//5,2
+        };
+
+        mesh.vertices = vertices;
+
+        int[] starBuilding = new int[]
+        {
+            1,4,2,0,3,1,
+        };
+
+        mesh.starBuilding = starBuildingIndices;
+
+        MeshFilter mf = this.AddComponent<MeshFilter>();
+        MeshRenderer mr = this.AddComponent<MeshRenderer>();
+
+        mf.mesh = mesh;
     }
+
 
     // Update is called once per frame
     void Update()
