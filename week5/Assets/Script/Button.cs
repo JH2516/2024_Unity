99c4413d27c121c85dcf52b7d3a829e20fd94c3d
JH2516 <kaizer2516@gmail.com>
2024-04-06 17:01:33 +0900

    week5 HW

--- week5/Assets/Script/Button.cs
+++ week5/Assets/Script/Button.cs
@@ -28,19 +28,93 @@ public class StaticMeshGen : MonoBehaviour
 
         Vector3[] vertices = new Vector3[]
         {
-            new Vector3 (Math.Cos(90), 0.0f, Math.Sin(90)),//1,4
-            new Vector3 (Math.Cos(162), 0.0f, Math.Sin(162)),//2,1,6
-            new Vector3 (Math.Cos(234), 0.0f, Math.Sin(234)),//3,3
-            new Vector3 (Math.Cos(306), 0.0f, Math.Sin(306)),//4,5
-            new Vector3 (Math.Cos(18), 0.0f, Math.Sin(18)),//5,2
+            //À­¸é
+            new Vector3 (Math.Cos(18), 0.0f, Math.Sin(18)),
+            new Vector3((0.5*Math.Cos(54)), 0.0f,((0.5* Math.Sin(54))),
+            new Vector3 (Math.Cos(90), 0.0f, Math.Sin(90)),
+            new Vector3((0.5*Math.Cos(126)), 0.0f,((0.5* Math.Sin(126))),
+            new Vector3 (Math.Cos(162), 0.0f, Math.Sin(162)),
+            new Vector3((0.5*Math.Cos(198)), 0.0f,((0.5* Math.Sin(198))),
+            new Vector3 (Math.Cos(234), 0.0f, Math.Sin(234)),
+            new Vector3((0.5*Math.Cos(270)), 0.0f,((0.5* Math.Sin(270))),
+            new Vector3 (Math.Cos(306), 0.0f, Math.Sin(306)),
+            new Vector3((0.5*Math.Cos(342)), 0.0f,((0.5* Math.Sin(342))),
+            //¾Æ·§¸é
+            new Vector3 (Math.Cos(18), 1.0f, Math.Sin(18)),
+            new Vector3((0.5*Math.Cos(54)), 1.0f,((0.5* Math.Sin(54))),
+            new Vector3 (Math.Cos(90), 1.0f, Math.Sin(90)),
+            new Vector3((0.5*Math.Cos(126)), 1.0f,((0.5* Math.Sin(126))),
+            new Vector3 (Math.Cos(162), 1.0f, Math.Sin(162)),
+            new Vector3((0.5*Math.Cos(198)), 1.0f,((0.5* Math.Sin(198))),
+            new Vector3 (Math.Cos(234), 1.0f, Math.Sin(234)),
+            new Vector3((0.5*Math.Cos(270)), 1.0f,((0.5* Math.Sin(270))),
+            new Vector3 (Math.Cos(306), 1.0f, Math.Sin(306)),
+            new Vector3((0.5*Math.Cos(342)), 1.0f,((0.5* Math.Sin(342))),
         };
 
         mesh.vertices = vertices;
 
-        int[] starBuilding = new int[]
+        int[] starBuildingVerteciesUp = new int[]
         {
-            1,4,2,0,3,1,
+            1,2,3,4,5,6,7,8,9,10,
         };
+        int[] starBuildingVertecisDown = new int[]
+        {
+            11,12,13,14,15,16,17,18,19,20,
+        };
+        int[] starBuildingVertecisSide1 = new int[]
+        {
+            1,2,11,12,
+        };
+        int[] starBuildingVertecisSide2 = new int[]
+        {
+            2,3,12,13,
+        };
+        int[] starBuildingVertecisSide3 = new int[]
+        {
+            3,4,13,14,
+        };
+        int[] starBuildingVertecisSide4 = new int[]
+        {
+            4,5,14,15,
+        };
+        int[] starBuildingVertecisSide5 = new int[]
+        {
+            5,6,15,16,
+        };
+        int[] starBuildingVertecisSide6 = new int[]
+        {
+            6,7,16,17,
+        };
+        int[] starBuildingVertecisSide7 = new int[]
+        {
+            7,8,17,18,
+        };
+        int[] starBuildingVertecisSide8 = new int[]
+        {
+            8,9,18,19,
+        };
+        int[] starBuildingVertecisSide9 = new int[]
+        {
+            9,10,19,20,
+        };
+        int[] starBuildingVertecisSide10 = new int[]
+        {
+            10,1,20,11,
+        };
+        int[] starBuildingIndices = new int[12];
+        starBuildingIndices[0] = starBuildingVerteciesUp;
+        starBuildingIndices[1] = starBuildingVertecisDown;
+        starBuildingIndices[2] = starBuildingVertecisSide1;
+        starBuildingIndices[3] = starBuildingVertecisSide2;
+        starBuildingIndices[4] = starBuildingVertecisSide3;
+        starBuildingIndices[5] = starBuildingVertecisSide4;
+        starBuildingIndices[6] = starBuildingVertecisSide5;
+        starBuildingIndices[7] = starBuildingVertecisSide6;
+        starBuildingIndices[8] = starBuildingVertecisSide7;
+        starBuildingIndices[9] = starBuildingVertecisSide8;
+        starBuildingIndices[10] = starBuildingVertecisSide9;
+        starBuildingIndices[11] = starBuildingVertecisSide10;
 
         mesh.starBuilding = starBuildingIndices;
 
